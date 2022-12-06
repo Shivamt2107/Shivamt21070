25bdd9d403621bbb774cde2198760a3494e1ebd8
Shivamt2107 <shivam.gote2107@gmail.com>
2022-12-06 19:27:37 +0530

    add change protfolio

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -2,6 +2,5 @@ export const aboutData = {
     title: "Who I am",
     description1: "My name's Shivam. I'm a full-stack web-developer based in Masai School, Benguluru.",
     description2:"I am a B.com graduate in Insurance, love exploring new technologies in the field of Web Development and always try to adapt to it.I believe in the concept of continuous learning by regularly upgrading my skills and enhancing my knowledge.I have learned MERN stack, data structures, algorithms, and soft skills at Masai School.And besides coding, I love traveling, singing and cooking As a developer, my hunger for learning has drastically increased. And I want to centralize my skills and learning for enhancement of an organization and more of it, to enhance my career.",
-   
     image: 1
 }
